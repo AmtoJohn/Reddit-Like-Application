cce269c0c8dc8d2292045dacc8543a8740a4b392
AmtoJohn <amto.john@iisgalvanimi.edu.it>
2022-12-16 12:57:48 +0000

    6.1

--- livera-reddit/src/app/article/article.component.spec.ts
+++ livera-reddit/src/app/article/article.component.spec.ts
@@ -1,18 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { ArticleComponent } from './article.component';
+import { AppComponent } from './article.component';
 
 describe('ArticleComponent', () => {
-  let component: ArticleComponent;
-  let fixture: ComponentFixture<ArticleComponent>;
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ArticleComponent ]
+      declarations: [ AppComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(ArticleComponent);
+    fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
